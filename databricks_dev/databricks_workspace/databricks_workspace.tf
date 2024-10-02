48ffaa76c0fb4c81a6e6331a981b16509ef79723
kohisha <kohisha.aruganti@celebaltech.com>
2024-10-02 19:02:03 -0400

    devlepoment

---
+++ databricks_dev/databricks_workspace/databricks_workspace.tf
@@ -0,0 +1,23 @@
+module "uc_catalog" {
+  source = "../../common_modules_workspace/uc_workspace_catalog"
+
+  databricks_account_id   = var.databricks_account_id
+  aws_account_id          = var.aws_account_id
+  resource_prefix         = var.resource_prefix
+  uc_catalog_name         = "${var.resource_prefix}-catalog-${var.workspace_id}"
+  workspace_id            = var.workspace_id
+  workspace_catalog_admin = var.workspace_catalog_admin
+  
+}
+
+module "dev_compute_policy" {
+  source = "../../common_modules_workspace/cluster_policy"
+  team   = var.team
+  policy_overrides = {
+    "spark_conf.spark.databricks.io.cache.enabled" : {
+      "type" : "fixed",
+      "value" : "true"
+    },
+  }
+}
+
---
+++ databricks_dev/workspace_env.tfvars
@@ -0,0 +1,37 @@
+// General 
+resource_prefix = "" 
+resource_owner = ""
+#access_key = ""
+#secret_key = ""
+
+
+// General - 
+aws_account_id = "" 
+region = ""
+
+// General - Databricks 
+databricks_account_id = "" 
+client_id = "" 
+client_secret = "" 
+metastore_id = ""
+
+
+availability_zones =  ""
+ private_subnets_cidr = ""
+ public_subnets_cidr = ""
+ sg_egress_ports = ""
+ sg_egress_protocol = ""
+ sg_ingress_protocol = ""
+ vpc_cidr_range = ""
+ 
+
+// Cloud Provider Storage (Root Storage) 
+dbfsname = ""
+
+// Workspace and Catalog Admin 
+user_name = 
+
+// Cluster Policy 
+team = ""
+
+
