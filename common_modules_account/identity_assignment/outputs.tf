ec4823ab6c6f7bc5a03f8a74dcc462acbb5b9fa8
kohisha <kohisha.aruganti@celebaltech.com>
2024-10-15 23:17:47 -0400

    adding user's in output for reference

--- common_modules_account/identity_assignment/outputs.tf
+++ common_modules_account/identity_assignment/outputs.tf
@@ -1,6 +1,21 @@
 output "dataengineer_group_name"{
     value = databricks_group.dataengineer.display_name
 }
+output "dataengineer_group_id"{
+    value = databricks_group.dataengineer.id
+}
+output "databricks_group_membership_user1" {
+  value = databricks_group_member.databricks_group_membership_user1.member_id
+}
+
+output "workspace_group_membership_user2" {
+  value = databricks_group_member.databricks_group_membership_user2.member_id
+}
+
+output "workspace_group_membership_user3" {
+  value = databricks_group_member.databricks_group_membership_user3.member_id
+}
+
 output "user1_id"{
     value = databricks_user.user1.id
 }
